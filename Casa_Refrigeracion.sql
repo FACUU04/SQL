e358b3733bb1057460ed0a2ff97c7ec82e16e74c
Facu <facundososa550@gmail.com>
2024-04-26 15:38:48 -0300

    Se agregaron sp, funciones, users y se corrijieron errores para poder tener una base de datos relacional eficiente

--- Casa_Refrigeracion.sql
+++ Casa_Refrigeracion.sql
@@ -1,4 +1,5 @@
-CREATE SCHEMA CASA_REFRIGERACION;
+DROP SCHEMA CASA_REFRIGERACION;
+CREATE SCHEMA IF NOT EXISTS CASA_REFRIGERACION;
 USE CASA_REFRIGERACION;
 
 CREATE TABLE IF NOT EXISTS NEGOCIO(
@@ -71,6 +72,14 @@ CREATE TABLE IF NOT EXISTS CLIENTES(
        DNI NUMERIC(8)
 );
 
+CREATE TABLE IF NOT EXISTS Registro_Actividad (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FechaHora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    TipoActividad ENUM('INSERT', 'UPDATE', 'DELETE'),
+    TablaAfectada VARCHAR(50),
+    Detalles TEXT
+);
+
 
 ALTER TABLE NEGOCIO
 ADD CONSTRAINT fk_empleados
@@ -98,34 +107,59 @@ FOREIGN KEY (ID_Producto) REFERENCES PRODUCTOS (ID_Producto);
  
  
  -- Esta vista muestra cuales son los productos mas vendidos junto con el importe total
-CREATE VIEW Productos_Mas_Vendidos AS
+CREATE VIEW VW_Productos_Mas_Vendidos AS
 SELECT p.ID_Producto, p.Descripcion, SUM(v.Importe) AS Importe_Total
 FROM PRODUCTOS p
 JOIN VENTAS v ON p.ID_Producto = v.IDPRODUCTO
 GROUP BY p.ID_Producto, p.Descripcion
 ORDER BY Importe_Total DESC;
 
+SELECT * FROM  Productos_Mas_Vendidos;
 
 -- Esta vista muestra cuales son los clientes mas frecuentes
-CREATE VIEW Clientes_Mas_Frecuentes AS
+CREATE VIEW VW_Clientes_Mas_Frecuentes AS
 SELECT c.ID_Cliente, CONCAT(c.Nombre, ' ', c.Apellido) AS Nombre_Completo, COUNT(*) AS Total_Compras
 FROM CLIENTES c
 JOIN VENTAS v ON c.ID_Cliente = v.IDCliente
 GROUP BY c.ID_Cliente, Nombre_Completo
 ORDER BY Total_Compras DESC;
 
+SELECT * FROM Clientes_Mas_Frecuentes;
+
 -- Esta vista muestra cuales son los productos con bajo STOCK
-CREATE VIEW Productos_Bajo_Stock AS
+CREATE VIEW VW_Productos_Bajo_Stock AS
 SELECT *
 FROM PRODUCTOS
 WHERE CantidadSTOCK < 10;
 
+SELECT * FROM VW_Productos_Bajo_Stock;
+
 -- Esta vista muestra la cantidad de ventas por mes
-CREATE VIEW Ventas_Por_Mes AS
-SELECT YEAR(Fecha) AS Anio, MONTH(Fecha) AS Mes, SUM(Importe) AS Total_Ventas
+CREATE VIEW VW_Ventas_Por_Mes AS
+SELECT YEAR(Fecha) AS Año, MONTH(Fecha) AS Mes, SUM(Importe) AS Importe_Total_Ventas
 FROM VENTAS
-GROUP BY Anio, Mes
-ORDER BY Anio DESC, Mes DESC;
+GROUP BY Año, Mes
+ORDER BY Año DESC, Mes DESC;
+
+SELECT * FROM VW_Ventas_Por_Mes;
+
+ -- Esta vista mostrara las ventas realizadas en el ultimo año
+CREATE VIEW VW_Ventas_Rango_Fechas AS
+SELECT Fecha, SUM(Importe) AS Total_Ventas
+FROM VENTAS
+WHERE Fecha BETWEEN '2024-01-01' AND '2025-01-01'  -- Rango de fechas deseado
+GROUP BY Fecha;
+
+CREATE VIEW VW_ClientesInactivos AS
+SELECT 
+    IDCliente,
+   Cantidad,
+   Importe,
+    Fecha
+FROM 
+    VENTAS
+WHERE 
+    Fecha < DATE_SUB(CURDATE(), INTERVAL 6 MONTH);
 
 
 CREATE USER 'JulianGutierrez'@'localhost' IDENTIFIED BY 'JG2022';
@@ -133,13 +167,13 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON CASA_REFRIGERACION.PRODUCTOS TO 'lectura
 
 
 CREATE USER 'Leon'@'localhost' IDENTIFIED BY '1994Alvarez';
-GRANT ALL PRIVILEGES ON CASA_REFRIGERACION.CLIENTES TO 'control_clientes'@'localhost';
+GRANT PRIVILEGES ON CASA_REFRIGERACION.CLIENTES TO 'control_clientes'@'localhost';
 
 
 CREATE USER 'Facundo'@'localhost' IDENTIFIED BY 'FalcarREFRIGERACION';
-GRANT ALL PRIVILEGES ON CASA_REFRIGERACION.EMPLEADOS TO 'control_compras'@'localhost';
-GRANT ALL PRIVILEGES ON CASA_REFRIGERACION.PROVEEDORES TO 'control_compras'@'localhost';
-GRANT ALL PRIVILEGES ON CASA_REFRIGERACION.COMPRAS TO 'control_compras'@'localhost';
+GRANT  PRIVILEGES ON CASA_REFRIGERACION.EMPLEADOS TO 'control_compras'@'localhost';
+GRANT  PRIVILEGES ON CASA_REFRIGERACION.PROVEEDORES TO 'control_compras'@'localhost';
+GRANT  PRIVILEGES ON CASA_REFRIGERACION.COMPRAS TO 'control_compras'@'localhost';
 
 
 CREATE USER 'Franco'@'localhost' IDENTIFIED BY 'FrancoFALCAR';
@@ -151,6 +185,158 @@ CREATE USER 'Alvaro'@'localhost' IDENTIFIED BY 'JKL890';
 GRANT SELECT, INSERT, UPDATE, DELETE ON CASA_REFRIGERACION.PROVEEDORES TO 'control_proveedores'@'localhost';
 GRANT SELECT ON CASA_REFRIGERACION.CATEGORIAS TO 'control_proveedores'@'localhost';
 GRANT SELECT ON CASA_REFRIGERACION.PRODUCTOS TO 'control_proveedores'@'localhost';
+
+
+/*El siguiente Trigger se activara despues de cada insercion, actualizacion o eliminacion en la tabla ventas
+realizando una insercion en la tabla Registro_Actividad con los cambios que se hicieron*/
+DELIMITER //
+CREATE TRIGGER TRG_Registro_actividad_
+AFTER INSERT ON VENTAS
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Actividad (TipoActividad, TablaAfectada, Detalles)
+    VALUES ('INSERT', 'VENTAS', CONCAT('Nuevo registro insertado: ', ID));
+END//
+
+DELIMITER ;
+
+/*El siguiente Trigger se activara despues de cada insercion en la tabla de ventas y compras, actualizando la cantidad de STOCK
+de los productos afectados segun la cantidad vendida o comprada*/
+DELIMITER //
+
+CREATE TRIGGER TRG_Actualizacion_stock
+AFTER INSERT ON VENTAS
+FOR EACH ROW
+BEGIN
+    UPDATE PRODUCTOS
+    SET CantidadSTOCK = CantidadSTOCK - NEW.Cantidad
+    WHERE ID_Producto = NEW.IDPRODUCTO;
+END//
+
+
+DELIMITER //
+CREATE TRIGGER TRG_Actualizacion_stock
+AFTER INSERT ON COMPRAS
+FOR EACH ROW
+BEGIN
+    UPDATE PRODUCTOS
+    SET CantidadSTOCK = CantidadSTOCK + NEW.Cantidad
+    WHERE ID_Producto = NEW.ID_Producto;
+END//
+
+DELIMITER ;
+
+/*El siguiente Trigger se activara despues de que se realize una venta de mas de 1 millon, cuando una venta superior a ese importe
+se realice, el trigger se activara y enviara un mail notificando que se acaba de realizar una venta grande */
+DELIMITER //
+
+CREATE TRIGGER TRG_Notificar_venta_grande
+AFTER INSERT ON VENTAS
+FOR EACH ROW
+BEGIN
+    IF NEW.Importe > 1000000 THEN
+        -- Venta grande, enviar notificación por correo electrónico
+        CALL SEND_EMAIL(IDCliente, 'ventas@falcar.com', 'Venta grande realizada', 'Se ha realizado una venta de más de $1.000.000.');
+    END IF;
+END//
+
+
+/*La siguiente funcion calcularia el total de ventas por cliente con el importe total gastado*/
+DELIMITER //
+CREATE FUNCTION FN_Calcular_total_ventas_cliente(cliente_id INT) RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+NO SQL
+BEGIN
+    DECLARE total DECIMAL(10, 2);
+    SELECT SUM(Importe) INTO total
+    FROM VENTAS
+    WHERE IDCliente = cliente_id;
+    RETURN total;
+END//
+DELIMITER ;
+
+SELECT Calcular_total_ventas_cliente (3);
+
+/*La siguiente funcion sirve para verificar si hay la cantidad de stock disponible de cierto producto para realizar la venta*/
+DELIMITER //
+CREATE FUNCTION FN_verificar_disponibilidad_producto_venta(producto_id INT, cantidad_deseada INT) RETURNS BOOLEAN
+READS SQL DATA
+BEGIN
+    DECLARE stock_actual INT;
+    SELECT CantidadSTOCK INTO stock_actual
+    FROM PRODUCTOS
+    WHERE ID_Producto = producto_id;
+    
+    IF stock_actual >= cantidad_deseada THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END//
+DELIMITER ;
+
+
+/*La siguiente funcion sirve para calcular la cantidad total de productos comprados durante el ultimo mes*/
+DELIMITER //
+
+CREATE FUNCTION FN_calcular_cantidad_total_productos_comprados_ultimo_mes(producto_id INT) RETURNS NUMERIC
+DETERMINISTIC
+BEGIN
+    DECLARE total_comprado NUMERIC;
+    DECLARE fecha_inicio DATE;
+    DECLARE fecha_fin DATE;
+    
+    SET fecha_inicio = DATE_SUB(LAST_DAY(CURRENT_DATE), INTERVAL 1 MONTH) + INTERVAL 1 DAY;
+    SET fecha_fin = LAST_DAY(CURRENT_DATE);
+    
+    SELECT SUM(Cantidad) INTO total_comprado
+    FROM COMPRAS
+    WHERE ID_Producto = producto_id
+    AND FECHA BETWEEN fecha_inicio AND fecha_fin;
+    
+    RETURN total_comprado;
+END//
+
+DELIMITER ;
+
+
+/*El siguiente sp servira para actualizar el STOCK de los productos */
+DELIMITER //
+CREATE PROCEDURE SP_Actualizar_stock(
+    IN ventas_id INT,
+    IN compras_id INT
+)
+BEGIN
+
+    UPDATE PRODUCTOS P
+    INNER JOIN VENTAS V ON P.ID_Producto = V.IDPRODUCTO
+    SET P.CantidadSTOCK = P.CantidadSTOCK - V.Cantidad
+    WHERE V.Nro_venta = ventas_id;
+    
+
+    UPDATE PRODUCTOS P
+    INNER JOIN COMPRAS C ON P.ID_Producto = C.ID_Producto
+    SET P.CantidadSTOCK = P.CantidadSTOCK + C.Cantidad
+    WHERE C.ID_Compra = compras_id;
+    
+ 
+    SELECT 'Stock actualizado correctamente' AS Mensaje;
+END;
+
+/*El siguiente procedimiento calcula la cantidad total de ventas por mes*/
+DELIMITER //
+CREATE PROCEDURE SP_Calcular_total_ventas_por_mes()
+BEGIN
+    -- Consulta para calcular el total de ventas por mes
+    SELECT YEAR(Fecha) AS Año, MONTH(Fecha) AS Mes, SUM(Importe) AS Total_Ventas
+    FROM VENTAS
+    GROUP BY Año, Mes;
+END;
+
+
+
+
+
 
 
 
